e2d88d0453c42594e4ae2f66984c954366fbca1e
yehmoonblue <moonblue.yeh@gmail.com>
2017-05-13 17:08:13 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
+var config = {
+  apiKey: "AIzaSyCLJl3kDjpnnO8F41n9j9rG_gdOjY_Abi8",
+  authDomain: "simpsonaccounter.firebaseapp.com",
+  databaseURL: "https://simpsonaccounter.firebaseio.com",
+  projectId: "simpsonaccounter",
+  storageBucket: "simpsonaccounter.appspot.com",
+  messagingSenderId: "472259175988"
+};
+firebase.initializeApp(config);
+
+
 var fdb = new ForerunnerDB();
 var db = fdb.db("資料庫");
 MoneyCollection = db.collection('moneys');
 MoneyCollection.load()
+
+
+var firebaseAuth = firebase.auth();
