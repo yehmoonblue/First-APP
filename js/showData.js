c760667a56d327b63422589a48efd5cf7c0a04c7
yehmoonblue <moonblue.yeh@gmail.com>
2017-05-13 16:56:08 +0800

    Update showData.js

--- js/showData.js
+++ js/showData.js
@@ -1,12 +1,1 @@
 var sum = [0,0,0,0,0,0]
-
-  MoneyCollection.load(function(){
-      $("table#commenttable tbody").text("");
-      
-      var result= MoneyCollection.find(
-        { },
-        { $orderBy: {date:-1},$limit: 10}
-        );
-        
-      for(var i=0;i<result.length;i++){
-        var commentdata="<tr><td>"+result[i].date+"</td><td>"+result[i].name+"</td><td>"+result[i].number+"</tr>"
