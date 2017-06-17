1fc35f33f0fc3247eb21e50d4beb51a0f92929e0
yehmoonblue <moonblue.yeh@gmail.com>
2017-06-17 16:57:03 +0800

    Update showData.js

--- js/showData.js
+++ js/showData.js
@@ -1,6 +1,5 @@
 var sum = [0,0,0,0,0,0]
 
-
 MoneyCollection.load(function(){
   $("table#commenttable tbody").text("");
   
@@ -9,29 +8,25 @@ MoneyCollection.load(function(){
     { $orderBy: {date:-1},$limit: 10}
   );
   
-    for(var i=0;i<result.length;i++){
-      var commentdata="<tr><td>"+result[i].name+"</td><td>"+result[i].number+"</td><td>"+result[i].date+"</td><td>"+"<button id='deleteitem' type='button' class='btn btn-default' data-id='"+MoneyCollection.find()[i]._id+"'>刪除</button>"+"</td></tr>"
-      $("table#commenttable tbody").append(commentdata);
-    };
+  
+  for(var i=0;i<result.length;i++){
+    var commentdata="<tr><td>"+result[i].name+"</td><td>"+result[i].number+"</td><td>"+result[i].date+"</td><td>"+"<button id='deleteitem' type='button' class='btn btn-default' data-id='"+MoneyCollection.find()[0]._id+"'>刪除</button>"+"</td></tr>"
+    $("table#commenttable tbody").append(commentdata);
+  };
 });
 
 
 function deleteitem(){
-      var id = $(this).data("id");
-      console.log(id)
-      var r=confirm("你確定要刪掉嗎？")
-      if(r){
-        MoneyCollection.remove({
-          _id:id
-        });  
-        MoneyCollection.save();
-      };
-        console.log(777);
-      }
-        
+  var id = $(this).data("id");
+  console.log(id)
+  var r=confirm("你確定要刪掉嗎？")
+  if(r){
+    MoneyCollection.remove({
+      _id:id
+    });  
+    MoneyCollection.save();
+  };
+  console.log(777);
+}
 
-
-
-      
-      $("table").on("click","button#deleteitem",deleteitem);
-      
+$("table").on("click","button#deleteitem",deleteitem);
