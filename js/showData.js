516d1e2b992afdc7a45c14ba322b7bf54de95226
yehmoonblue <moonblue.yeh@gmail.com>
2017-06-24 14:25:56 +0800

    Update showData.js

--- js/showData.js
+++ js/showData.js
@@ -7,7 +7,7 @@ MoneyCollection.load(function(){
     { },
     { $orderBy: {date:-1},$limit: 10}
   );
-  
+  function showitem(){
   
   for(var i=0;i<result.length;i++){
     var commentdata="<tr><td>"+result[i].name+"</td><td>"+result[i].type+"</td><td>"+result[i].number+"</td><td>"+result[i].date+"</td><td>"+"<button id='deleteitem' type='button' class='btn btn-default' data-id='"+MoneyCollection.find()[i]._id+"'>刪除</button>"+"</td></tr>"
@@ -28,19 +28,19 @@ MoneyCollection.load(function(){
       default:
         this.sum[4] = this.sum[4] + result[i].number/1
         break;
-                         }
+                         }}
   };
 });
 
 function deleteitem(){
   var id = $(this).data("id");
-  console.log(id)
   var r=confirm("你確定要刪掉嗎？")
   if(r){
     MoneyCollection.remove({
       _id:id
     });  
     MoneyCollection.save();
+    showitem();
   };
   console.log(777);
 }
